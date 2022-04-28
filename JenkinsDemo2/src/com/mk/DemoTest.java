9c753999fe683d47f937648f5c90f034d5c9de98
Manish <Manish@Manish>
2022-04-28 14:35:36 +0530

    by rajesh

---
+++ JenkinsDemo2/src/com/mk/DemoTest.java
@@ -0,0 +1,23 @@
+package com.mk;
+
+public class DemoTest {
+	
+	public static void main(String[] args) {
+		System.out.println("hiiii");
+		
+		DemoTest d = new DemoTest();
+		d.m1();
+		d.m2();
+		
+		
+	}
+	
+	public void m1() {
+		System.out.println("M1 method");
+	}
+	
+	public void m2() {
+		System.out.println("M2 method");
+	}
+
+}
